e0e805f767b76caa10fe636b46b8c09d768cf1be
Andrew Engel <androidengel@gmail.com>
2021-04-09 15:24:50 -0500

    adding all nav components, styling and media queries

--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,111 @@
 import styled from 'styled-components';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const NavStyles = styled.nav`
+  border-bottom: var(--card-border);
+  box-shadow: var(--bs);
+  position: fixed;
+  background: white;
+  width: 100%;
+  min-height: var(--nav-height);
+  display: flex;
+  justify-content: space-evenly;
+  color: var(--blue);
+  a {
+    color: var(--blue);
+  }
+  #logo {
+    /* margin-right: auto;
+    padding-left: 2rem; */
+  }
+  .phone-number {
+    margin-left: 4rem;
+    white-space: nowrap;
+    margin: auto 0;
+    padding: 0 2rem;
+    font-weight: bold;
+    & p {
+      margin: 0;
+      padding: 0 2rem;
+      line-height: 1px;
+      text-align: center;
+    }
+    & a {
+      font-size: 2.6rem;
+      font-weight: bold;
+      color: var(--orange);
+      text-decoration: none;
+    }
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    text-align: center;
+    justify-content: center;
+    
+    #logo {
+      width: 100%;
+      margin: auto;
+      padding: 0;
+    }
+    .phone-number {
+      p {
+        /* padding: 2rem 0 0 0; */
+        text-align: center;
+      }
+    }
+
+  }
+`;
+
+const NavLinks = styled.ul`
+  list-style: none;
+  display: flex;
+  margin: auto 0;
+  li {
+    margin: auto 0;
+    padding-left: 4rem;
+    font-weight: bold;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
 
 const Nav = () => {
   const variable = '';
 
   return (
-    <nav>
-      <ul>
-        <li>About</li>
-        <li>Call us! 920-315-0080</li>
-      </ul>
-    </nav>
+    <NavStyles>
+      <div id="logo">
+        <Link href="/">
+          <a>
+            <Image src="/logo.png" alt="fast selling solutions" width={240} height={97} />
+          </a>
+        </Link>
+      </div>
+      <div className="phone-number">
+        <p>Call or text!</p>
+        <a className="phone-number" href="tel:920-315-0080">920-315-0080</a>
+      </div>
+      <NavLinks>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/refer">
+            <a>Refer</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/faq">
+            <a>FAQ</a>
+          </Link>
+        </li>
+      </NavLinks>
+    </NavStyles>
   );
 };
 
