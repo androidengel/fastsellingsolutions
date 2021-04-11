836556f17853039980ab5f6ac39050d82cffcc6f
Andrew Engel <androidengel@gmail.com>
2021-04-11 06:00:01 -0500

    adding react-places-autocomplete

--- components/Form.js
+++ components/Form.js
@@ -1,4 +1,9 @@
 import styled from 'styled-components';
+import { useState } from 'react';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
 import useForm from '../hooks/useForm';
 
 const FormStyles = styled.form`
@@ -19,7 +24,7 @@ const FormStyles = styled.form`
   label {
     margin-bottom: 1rem;
     font-weight: bold;
-    & span {
+    & span.asterisk {
       color: var(--red);
     }
   }
@@ -30,6 +35,28 @@ const FormStyles = styled.form`
       cursor: pointer;
       color: var(--light-blue-hover);
     }
+  }
+  .location-search-input,
+  .location-search-input:focus,
+  .location-search-input:active {
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.16), 0 0 0 1px rgba(0,0,0,0.08);
+    border: honeydew;
+    display: block;
+    width: 100%;
+    padding: 16px;
+    font-size: 16px;
+    border-radius: 2px;
+    outline: none;
+  }
+  .autocomplete-dropdown-container {
+    border-bottom: honeydew;
+    border-left: honeydew;
+    border-right: honeydew;
+    border-top: 1px solid #e6e6e6;
+    box-shadow: 0 2px 4px rgba(0,0,0,0.2);
+    position: absolute;
+    z-index: 1000;
+    border-radius: 0 0 2px 2px;
   }
   button {
     margin-top: 2rem;
@@ -46,7 +73,12 @@ const FormRow = styled.div`
 `;
 
 const Form = () => {
-  const { inputs, handleChange, clearForm } = useForm();
+  const { inputs, handleChange, clearForm } = useForm({ address: '' });
+  const [address, setAddress] = useState();
+
+  const handleAddressChange = (addr) => {
+    setAddress(addr);
+  };
 
   return (
     <FormStyles>
@@ -54,18 +86,56 @@ const Form = () => {
         <h2>Tell us about your property!</h2>
         <label htmlFor="address" name="address">
           Property address
-          <span>*</span>
-          <input type="text" name="address" value={inputs.address} onChange={handleChange} required />
+          <span className="asterisk">*</span>
+          <PlacesAutocomplete
+            value={address}
+            onChange={handleAddressChange}
+          >
+            {({
+              getInputProps, suggestions, getSuggestionItemProps, loading,
+            }) => (
+              <div>
+                <input
+                  {...getInputProps({
+                    placeholder: 'Search Property ...',
+                    className: 'location-search-input',
+                  })}
+                />
+                <div className="autocomplete-dropdown-container">
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map((suggestion) => {
+                    const className = suggestion.active
+                      ? 'suggestion-item--active'
+                      : 'suggestion-item';
+                    // inline style for demonstration purpose
+                    const style = suggestion.active
+                      ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                      : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                    return (
+                      <div
+                        {...getSuggestionItemProps(suggestion, {
+                          className,
+                          style,
+                        })}
+                      >
+                        <span>{suggestion.description}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </PlacesAutocomplete>
         </label>
         <FormRow>
           <label htmlFor="email" name="email">
             Email
-            <span>*</span>
+            <span className="asterisk">*</span>
             <input type="email" name="email" value={inputs.email} onChange={handleChange} required />
           </label>
           <label htmlFor="phone" name="phone">
             Phone
-            <span>*</span>
+            <span className="asterisk">*</span>
             <input type="phone" name="phone" value={inputs.phone} onChange={handleChange} required />
           </label>
         </FormRow>
--- components/Meta.js
+++ components/Meta.js
@@ -5,7 +5,7 @@ const Meta = () => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <link rel="shortcut icon" href="/fss_icon.svg" crossOrigin="anonymous" />
-    <link rel="stylesheet" type="text/css" href="/nprogress.css" crossOrigin="anonymous" />
+    <script async src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_MAPS_KEY}&libraries=places&callback=initMap`} />
     <title>Fast Selling Solutions</title>
   </Head>
 );
