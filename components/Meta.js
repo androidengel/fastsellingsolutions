8f0bba0465afa1d00e8f7c479815e07ee064211a
Andrew Engel <androidengel@gmail.com>
2021-04-23 21:03:18 -0500

    adding favicon

--- components/Meta.js
+++ components/Meta.js
@@ -4,7 +4,7 @@ const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
-    <link rel="shortcut icon" href="/fss_icon.svg" crossOrigin="anonymous" />
+    <link rel="shortcut icon" href="/favicon.png" crossOrigin="anonymous" />
     <script async src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_MAPS_KEY}&libraries=places&callback=initMap`} />
     <title>Fast Selling Solutions</title>
   </Head>
