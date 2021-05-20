1397d3521a623d5fb1872f614420642add9f9087
Andrew Engel <androidengel@gmail.com>
2021-05-20 06:10:58 -0500

    adding api route to submit property contact us form

---
+++ pages/api/propertySubmission.js
@@ -0,0 +1,28 @@
+import nodemailer from 'nodemailer';
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 587,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+console.log(process.env.MAIL_HOST);
+
+export default async function handler(req, res) {
+  // send mail
+  const info = await transporter.sendMail({
+    from: 'Terold Phillips <terold@example.com>',
+    to: 'properties@example.com',
+    subject: 'sell my house!',
+    html: '<p>Please sell my house now!</p>',
+  });
+  res.status(200).json(info);
+}
+
+// export default (req, res) => {
+//   res.status(200).json({ name: 'John Doe' });
+// };
