e4b014263c43fb3a57a6b52c1a2af5b94709aa88
Andrew Engel <androidengel@gmail.com>
2021-07-11 13:38:51 -0500

    removing async from api script tag

--- components/Meta.js
+++ components/Meta.js
@@ -5,7 +5,7 @@ const Meta = () => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <link rel="shortcut icon" href="/favicon.png" crossOrigin="anonymous" />
-    <script async src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_MAPS_KEY}&libraries=places`} />
+    <script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_MAPS_KEY}&libraries=places`} />
     <title>Fast Selling Solutions | Selling your house made quick and easy</title>
   </Head>
 );
