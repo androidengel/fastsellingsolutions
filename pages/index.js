96403260fadf1c858971ea2855be2c9a5dc984d9
Andrew Engel <androidengel@gmail.com>
2021-07-13 06:15:49 -0500

    adding language to html tag and met description to index for seo

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import Home from '../components/Home/Home';
 
 export default function HomePage() {
   return (
     <div>
+      <Head>
+        <meta name="description" content="We are professional home buyers who specialize in making your house-selling process fast and easy. We buy houses in Wisconsin fast!" />
+      </Head>
       <main>
         <Home />
       </main>
